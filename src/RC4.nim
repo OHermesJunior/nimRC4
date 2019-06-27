8f083ff5a0ee6add0255349053c9c2dc092d6a85
OHermesJunior <34722728+OHermesJunior@users.noreply.github.com>
2019-06-27 19:09:55 +0200

    Decryption implemented

--- src/RC4.nim
+++ src/RC4.nim
@@ -1,14 +1,12 @@
 import strutils
 
-proc RC4*(key, data: string): string =
-  var converted_key, converted_data: seq[int]
+proc genKeystream(key: string): array[256, int] =
+  var converted_key: seq[int]
   var state: array[256, int]
   for i in 0..255:
     state[i] = i
   for c in key:
     converted_key.add(ord(c))
-  for c in data:
-    converted_data.add(ord(c))
 
   var j, h = 0
   for i in 0..255:
@@ -17,9 +15,15 @@ proc RC4*(key, data: string): string =
     state[i] = state[j]
     state[j] = temp
     h = (h + 1) mod key.len
+  return state
 
-  j = 0
-  h = 0
+proc toRC4*(key, data: string): string =
+  var converted_data: seq[int]
+  for c in data:
+    converted_data.add(ord(c))
+
+  var state = genKeystream(key)
+  var j, h = 0
 
   for c in converted_data:
     j = (j + 1) mod 256
@@ -28,3 +32,16 @@ proc RC4*(key, data: string): string =
     state[h] = state[j]
     state[j] = temp
     result.add((c xor state[(state[j] + state[h]) mod 256]).toHex(2))
+
+proc fromRC4*(key, data: string): string =
+  var state = genKeystream(key)
+  var i, j, h = 0
+
+  while i < data.len:
+    j = (j + 1) mod 256
+    h = (h + state[j]) mod 256
+    var temp = state[h]
+    state[h] = state[j]
+    state[j] = temp
+    result.add(char((fromHex[int](data[i] & data[i+1]) xor state[(state[j] + state[h]) mod 256])))
+    i += 2
--- tests/test1.nim
+++ tests/test1.nim
@@ -3,6 +3,10 @@
 import unittest
 
 import RC4
-test "rc4 encryption":
-  doAssert RC4("Wiki", "pedia") == "1021BF0420"
-  doAssert RC4("Key", "Plaintext") == "BBF316E8D940AF0AD3"
+test "RC4 Encryption":
+  doAssert toRC4("Wiki", "pedia") == "1021BF0420"
+  doAssert toRC4("Key", "Plaintext") == "BBF316E8D940AF0AD3"
+
+test "RC4 Decryption":
+  doAssert fromRC4("Wiki", "1021BF0420") == "pedia"
+  doAssert fromRC4("Key", "BBF316E8D940AF0AD3") == "Plaintext"
